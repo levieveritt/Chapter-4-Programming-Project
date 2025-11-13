71f90bfdb82fc82cb4c3a290c1188e3c24ad0d0e
levieveritt <lhideki169@gmail.com>
2025-11-13 04:04:55 +0000

    patterns

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <vector>
-
-using namespace std;
 
 int main() {
-    string townName;
-    cout << "Enter the name of the town: ";
-    getline(cin, townName);
-
-    vector<int> populationData = {2000, 4000, 5000, 9000, 14000, 18000};
-    int startYear = 2000; // Assuming the data starts from the year 2000
-
-    cout << "Population Growth Chart for " << townName << endl;
-
-    for (size_t i = 0; i < populationData.size(); ++i) {
-        int year = startYear + i;
-        int population = populationData[i];
-        int numAsterisks = population / 1000;
-
-        cout << year << ": ";
-        for (int j = 0; j < numAsterisks; ++j) {
-            cout << "*";
+    // Pattern A
+    for (int i = 1; i <= 10; ++i) {
+        for (int j = 0; j < i; ++j) {
+            std::cout << "+";
         }
-        cout << endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl; // newline
+
+    // Pattern B
+    for (int i = 10; i >= 1; --i) {
+        for (int k = 10; k > i; --k) {
+            std::cout << " ";
+        }
+        for (int j = 0; j < i; ++j) {
+            std::cout << "+";
+        }
+        std::cout << std::endl;
     }
 
     return 0;
