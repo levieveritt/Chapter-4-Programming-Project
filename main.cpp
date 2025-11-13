b55e0a616bae4ce6ecabd9c21d66f01a01931307
levieveritt <lhideki169@gmail.com>
2025-11-13 03:48:18 +0000

    student lineup

--- main.cpp
+++ main.cpp
@@ -1,37 +1,16 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 
-using namespace std;
-
-
+// update
 int main() {
-    int year, month;
+    std::vector<std::string> names = {"Barb", "Sam", "Tom", "Jackie", "Mary", "Paul", "Bill", "Zev"};
+    std::sort(names.begin(), names.end());
 
-    cout << "Enter year: ";
-    cin >> year;
-
-    cout << "Enter month (1-12): ";
-    cin >> month;
-
-    // Validate month input
-    if (month < 1 || month > 12) {
-        cout << "Invalid month. Month should be between 1 and 12." << endl;
-        return 1; // Indicate an error
-    }
-
-    bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-
-    int daysInMonth;
-
-    if (month == 2) {
-        daysInMonth = isLeapYear ? 29 : 28;
-    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-        daysInMonth = 30;
-    } else {
-        daysInMonth = 31;
-    }
-
-    cout << "Number of days in " << month << "/" << year << ": " << daysInMonth << endl;
+    std::cout << "Number of students: " << names.size() << std::endl;
+    std::cout << "First student: " << names.front() << std::endl;
+    std::cout << "Last student: " << names.back() << std::endl;
 
     return 0;
 }
