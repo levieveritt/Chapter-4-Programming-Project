8b76afda9de16d1f5af4289b0de99ffc3ee20cc6
levieveritt <lhideki169@gmail.com>
2025-11-13 04:26:00 +0000

    random number gen

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
-#include <string>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
-    // Pattern A
-    for (int i = 1; i <= 10; ++i) {
-        for (int j = 0; j < i; ++j) {
-            std::cout << "+";
-        }
-        std::cout << std::endl;
-    }
+    // random number generator
+    std::srand(std::time(0));
 
-    std::cout << std::endl; // newline
+    // generate random number 1 - 100
+    int randomNumber = std::rand() % 100 + 1;
 
-    // Pattern B
-    for (int i = 10; i >= 1; --i) {
-        for (int k = 10; k > i; --k) {
-            std::cout << " ";
+    int guess;
+    int guessCount = 0;
+
+    std::cout << "I'm thinking of a number between 1 and 100. Can you guess it?" << std::endl;
+
+    do {
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+        guessCount++;
+
+        if (guess > randomNumber) {
+            std::cout << "Too high, try again." << std::endl;
+        } else if (guess < randomNumber) {
+            std::cout << "Too low, try again." << std::endl;
+        } else {
+            std::cout << "Congratulations! You guessed the number in " << guessCount << " guesses." << std::endl;
         }
-        for (int j = 0; j < i; ++j) {
-            std::cout << "+";
-        }
-        std::cout << std::endl;
-    }
+    } while (guess != randomNumber);
 
     return 0;
 }
